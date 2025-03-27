ce7f51ea67206f7414cc52dd1aad58fbf2170664
Relyful <13381270+Relyful@users.noreply.github.com>
2025-03-27 21:08:15 +0100

    Install dotenv, setup basic express APP

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Ayo');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}, enjoy! ;)`);
+});
