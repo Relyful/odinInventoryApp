8c367754387b0b084ad888587c56d4ff30540529
Relyful <13381270+Relyful@users.noreply.github.com>
2025-04-07 20:32:53 +0200

    Fix viewCategory render

--- Controllers/categoryController.js
+++ Controllers/categoryController.js
@@ -1,4 +1,5 @@
-exports.indexGet = (req, res) => {
+exports.indexGet = async (req, res) => {
+  const { rows } = await pool.query("SELECT name FROM category");
   res.render('category', { categories: [
     {name: 'Hardtail'},
     {name: 'Road'},
@@ -6,8 +7,21 @@ exports.indexGet = (req, res) => {
   ]});
 }
 
-exports.categoryGet = (req, res) => {  
+exports.categoryGet = async (req, res) => {
   const cat = req.params.category;
+  const { rows } = await pool.query("SELECT * FROM bikes WHERE category = (SELECT id FROM category WHERE name = $(1))", [cat])
   console.log(cat);
-  res.send(`Hello ${cat}`);
+  res.render('viewCategory', { 
+    catName: cat, 
+    bikes: [
+      {
+        name: 'Zephyr Pro',
+        speeds: 12,
+        quantity: 420,
+        price: 1337,
+        brand: CTM,
+        category: cat
+      }
+    ]
+  });
 }
