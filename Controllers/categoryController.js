96e4fa092281589880052c5182727de23d8ef0f4
Relyful <13381270+Relyful@users.noreply.github.com>
2025-04-09 22:17:09 +0200

    Get category working fully with DB

--- Controllers/categoryController.js
+++ Controllers/categoryController.js
@@ -1,40 +1,31 @@
 const pool = require("../db/pool");
 
 exports.indexGet = async (req, res) => {
-  const { rows } = await pool.query("SELECT name FROM category");
-  res.render('category', { categories: [
-    {name: 'Hardtail'},
-    {name: 'Road'},
-    {name: 'Full suspension'}
-  ]});
+  const { rows } = await pool.query("SELECT cat_name FROM category");
+  res.render("category", {
+    categories: rows
+  });
 };
 
 exports.categoryGet = async (req, res) => {
   const cat = req.params.category;
-  const { rows } = await pool.query("SELECT * FROM bikes WHERE category = (SELECT id FROM category WHERE name = ($1))", [cat]);
+  const { rows } = await pool.query(
+    "SELECT bikes.*, brands.brand_name, category.cat_name FROM bikes JOIN category on bikes.category_id = category.id JOIN brands on bikes.brand_id = brands.id WHERE category_id = (SELECT id FROM category WHERE category.cat_name = ($1))",
+    [cat]
+  );
   console.log(rows);
-  console.log(cat);
-  res.render('viewCategory', { 
-    catName: cat, 
-    bikes: [
-      {
-        name: 'Zephyr Pro',
-        speeds: 12,
-        quantity: 420,
-        price: 1337,
-        brand: CTM,
-        category: cat
-      }
-    ]
+  res.render("viewCategory", {
+    catName: cat,
+    bikes: rows,
   });
 };
 
 exports.newCategoryGet = (req, res) => {
-  res.render('createCategory');
+  res.render("createCategory");
 };
 
 exports.newCategoryPost = async (req, res) => {
   const data = req.body.catName;
-  await pool.query("INSERT INTO category (catname) VALUES ($1)", [data]);
+  await pool.query("INSERT INTO category (cat_name) VALUES ($1)", [data]);
   res.redirect(`/category/${data}/bikes`);
-}
+};
