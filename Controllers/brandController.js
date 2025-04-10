726075c28404d4abf5fe3e798b7f5a4bbd3e8d87
Relyful <13381270+Relyful@users.noreply.github.com>
2025-04-10 20:30:16 +0200

    Move queries from brandController to db

--- Controllers/brandController.js
+++ Controllers/brandController.js
@@ -1,29 +1,28 @@
 const pool = require('../db/pool');
+const db = require('../db/queries');
 
 exports.indexGet = async (req, res) => {
-  const { rows } = await pool.query("SELECT brand_name from brands"); //Apply rows to render
+  const brandNames = await db.getBrandNames(); //Apply rows to render
   res.render('brands', { 
-    brands: rows,
+    brands: brandNames,
   });
-}
+};
 
 exports.brandGet = async (req, res) => {
   const brand = req.params.brand;
-  const { rows } = await pool.query("SELECT bikes.*, category.cat_name, brands.brand_name FROM bikes JOIN brands ON bikes.brand_id = brands.id JOIN category ON bikes.category_id = category.id WHERE brand_id = (SELECT id FROM brands WHERE brand_name = ($1))", [brand]); // apply rows
-  console.log(rows);
+  const bikes = await db.getAllBrandBikes(brand);
   res.render('viewBrand', {
     brandName: brand,
-    bikes: rows
+    bikes: bikes
   });
 };
 
 exports.newBrandGet = (req, res) => {
   res.render('createBrand');
-}
+};
 
 exports.newBrandPost = async (req, res) => {
   const data = req.body;
-  await pool.query(`INSERT INTO brands (brand_name, country)
-    VALUES ($1, $2)`, [data.brandName, data.brandCountry]);
+  await db.postNewBrand(data);
   res.redirect('/brand');
 };
--- db/queries.js
+++ db/queries.js
@@ -10,7 +10,20 @@ async function getBrandNames() {
   return rows;
 };
 
+async function getAllBrandBikes(brand) {
+  const { rows } = await pool.query("SELECT bikes.*, category.cat_name, brands.brand_name FROM bikes JOIN brands ON bikes.brand_id = brands.id JOIN category ON bikes.category_id = category.id WHERE brand_id = (SELECT id FROM brands WHERE brand_name = ($1))", [brand]);
+  return rows;
+};
+
+async function postNewBrand(data) {
+  await pool.query(`INSERT INTO brands (brand_name, country)
+      VALUES ($1, $2)`, [data.brandName, data.brandCountry]);
+      return;
+};
+
 module.exports = {
   getCategoryNames,
   getBrandNames,
+  getAllBrandBikes,
+  postNewBrand,
 };
