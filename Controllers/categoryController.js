509f4d0232e517aab6be57e6b25277d6a8505627
Relyful <13381270+Relyful@users.noreply.github.com>
2025-04-16 21:11:25 +0200

    Add form validation for category

--- Controllers/categoryController.js
+++ Controllers/categoryController.js
@@ -1,11 +1,21 @@
 const pool = require("../db/pool");
-const db = require('../db/queries');
-const asyncHandler = require('express-async-handler');
+const db = require("../db/queries");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+const categoryValidation = [
+  body("catName")
+    .trim()
+    .notEmpty()
+    .withMessage("Cannot be Empty.")
+    .isAscii()
+    .withMessage("Cannot contain special characters."),
+];
 
 exports.indexGet = asyncHandler(async (req, res) => {
   const categories = await db.getCategoryNames();
   res.render("category", {
-    categories
+    categories,
   });
 });
 
@@ -20,40 +30,59 @@ exports.categoryGet = asyncHandler(async (req, res) => {
 
 exports.newCategoryGet = (req, res) => {
   res.render("createCategory", {
-    action: 'Create',
-    defVal: '',
+    action: "Create",
+    defVal: "",
   });
 };
 
-exports.newCategoryPost = asyncHandler(async (req, res) => {
-  const data = req.body.catName;
-  await db.postNewCategory(data);
-  res.redirect(`/category`);
-});
+exports.newCategoryPost = [
+  categoryValidation,
+  asyncHandler(async (req, res) => {
+    const data = req.body.catName;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).send(errors);
+      return;
+    }
+    await db.postNewCategory(data);
+    res.redirect(`/category`);
+  }),
+];
 
 exports.updateCategoryGet = asyncHandler(async (req, res) => {
   const categoryId = req.params.catId;
   const category = await db.getCategoryFromId(categoryId);
-  if(!category) {
-    throw new Error("Category does not exist!")
+  if (!category) {
+    throw new Error("Category does not exist!");
   }
-  res.render('createCategory', {
-    action: 'Update',
+  res.render("createCategory", {
+    action: "Update",
     defVal: category.cat_name,
   });
 });
 
-exports.updateCategoryPost = asyncHandler(async (req, res) => {
-const categoryId = req.params.catId;
-const categoryName = req.body.catName;
-await pool.query(`UPDATE category
-  SET cat_name = $2
-  WHERE id = $1`, [categoryId, categoryName]);
-res.redirect('/category');
-});
+exports.updateCategoryPost = [
+  categoryValidation,
+  asyncHandler(async (req, res) => {
+    const categoryId = req.params.catId;
+    const categoryName = req.body.catName;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.sendStatus().send(errors);
+      return;
+    }
+    await pool.query(
+      `UPDATE category
+    SET cat_name = $2
+    WHERE id = $1`,
+      [categoryId, categoryName]
+    );
+    res.redirect("/category");
+  })
+];
 
 exports.deleteCategoryGet = asyncHandler(async (req, res) => {
   const categoryId = req.params.categoryId;
-  await pool.query('DELETE FROM category WHERE id = $1', [categoryId]);
-  res.redirect('/category');
-})
+  await pool.query("DELETE FROM category WHERE id = $1", [categoryId]);
+  res.redirect("/category");
+});
